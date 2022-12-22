77ade21d4f99b09ac79853bccede132c534b9dec
YesunPark <lynn08082@gmail.com>
2022-12-22 14:02:40 +0900

    Design: global-style 추가
    - src > styles 폴더에 global-styles.jsx추가
    - 메인컬러, 폰트, 사이즈 등 설정

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Router from './Router';
+import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
+import GlobalStyle from './styles/global-styles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <Router />
     </ThemeProvider>
   </React.StrictMode>
