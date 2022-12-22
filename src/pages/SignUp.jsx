e502594780e6e5e3ec800b5b60858bd6135e9374
YesunPark <lynn08082@gmail.com>
2022-12-22 14:32:04 +0900

    Add: 회원가입 ui 추가
    - 공통 스타일폼 사용해서 생성
    - 비밀번호 확인 input 한 개 추가

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,18 @@
+import { useState } from 'react';
+import LoginSignUpForm from '../components/common/LoginSignUpForm';
+
 const SignUp = () => {
-  return <>sdf</>;
+  const [input, setInput] = useState({
+    emailInput: '',
+    pwInput: '',
+    pwCheck: '',
+  });
+
+  const clickSignUpBtn = () => {};
+
+  return (
+    <LoginSignUpForm title='회원가입' input={input} setInput={setInput} btnClick={clickSignUpBtn} />
+  );
 };
 
 export default SignUp;
