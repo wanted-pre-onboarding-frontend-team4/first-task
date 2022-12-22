58edeb8064ae726a7b7185a5bbf6b848c2a3da1f
tlsrb100 <tlsrb100@gmail.com>
2022-12-22 15:27:57 +0900

    Feat: todoList CRUD api 추가

---
+++ src/apis/todo.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+  baseURL: process.env.REACT_BASE_URL,
+});
+
+const createTodoApi = async (body) => {
+  const { resStatus } = await apiClient.post('/todos', body);
+  return resStatus;
+};
+
+const getTodoApi = async () => {
+  const { todoList } = await apiClient.get('/todos');
+  return todoList;
+};
+
+const updateTodoApi = async (body, id) => {
+  const { resStatus } = await apiClient.put(`/todos/${id}`, body);
+  return resStatus;
+};
+
+const deleteTodoApi = async (id) => {
+  const { resStatus } = await apiClient.delete(`/todos/${id}`);
+  return resStatus;
+};
+
+export { createTodoApi, getTodoApi, updateTodoApi, deleteTodoApi };
