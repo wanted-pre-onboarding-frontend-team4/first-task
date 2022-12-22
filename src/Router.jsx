e47d2c64999db0b1e0c5518e0f995074723a3c15
YesunPark <lynn08082@gmail.com>
2022-12-22 14:15:26 +0900

    Design: 공통으로 사용할 Box.Style, Title 컴포넌트 생성
    - fontawesome 설치

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Title from "./components/common/Title";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import ToDo from "./pages/ToDo";
@@ -6,6 +7,7 @@ import ToDo from "./pages/ToDo";
 function Router() {
   return (
     <BrowserRouter>
+      <Title />
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
